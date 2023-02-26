187872922ead9583d087246546a04fe008ee11da
KIMSEI1124 <workju1124@gmail.com>
2023-02-26 23:02:29 +0900

    study: react props

---
+++ 리액트를 다루는 기술/Ch03/component/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './App.css';
+import MyComponent from './myComponent';
+
+function App() {
+  return <MyComponent name="React" name2="리액트" />;
+}
+
+export default App;
